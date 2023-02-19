b5cec9a3228ca875d73d1a0c3de82026c9ca5152
Laurentino <matheus.laurentino.ifpr@gmail.com>
2023-02-18 22:37:00 -0300

    express session finished

--- 8.Calculator (session1 challenge3)/calculator.js
+++ 8.Calculator (session1 challenge3)/calculator.js
@@ -1,9 +1,48 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// ------------------------
+
 
 app.get("/", function(req, res){
-    res.send("Hellasdsaoo");
+    res.sendFile(__dirname + "/index.html");
+    //console.log("diretorio: " + __dirname);
 });
+
+app.post("/", function(req, res){
+    //console.log(req.body);
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    res.send("O resultado é: " + result);
+});
+
+
+// ------------------------
+
+
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res){
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / (height * height);
+    res.send("BMI= " + bmi); 
+});
+
+
+// ------------------------
+
 
 app.listen(3000, function() {
     console.log("porta 3000");
