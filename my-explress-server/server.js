47486dc736c9da160f2e24e132294f3312c9c2e5
Laurentino <matheus.laurentino.ifpr@gmail.com>
2023-03-07 16:16:19 -0300

    adicionando comentarios

--- my-explress-server/server.js
+++ my-explress-server/server.js
@@ -1,21 +1,23 @@
-//jshint esversion:6
 
+// inicializa o express
 const express = require("express");
 const app = express();
 
-app.get("/", function(req, res){
-    //console.log(req);
-    res.send("Hello");
-});
+// Rotas
+    app.get("/", function(req, res){
+        res.send("Hello");
+        // é possivel enviar arquivos por meio do sendFile(__dir + "name.html")
+    });
 
-app.get("/contact", function(req, res){
-    res.send("Entre em contato por matheus.brasil@gmail.com")
-});
+    app.get("/contact", function(req, res){
+        res.send("Entre em contato por matheus.brasil@gmail.com")
+    });
 
-app.get("/about", function(req, res){
-    res.send("Me chamo matheus")
-});
+    app.get("/about", function(req, res){
+        res.send("Me chamo matheus")
+    });
 
+// Inicia o servidor na porta 3000
 app.listen(3000, function() {
     console.log("porta 3000");
 });
